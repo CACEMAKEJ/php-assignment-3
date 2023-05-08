adc5da82182bf2ef375e2d39af102936c47570e3
Manin Macina <Feedesson12@gmail.com>
2023-05-08 14:44:17 +0100

    feat: coaching-page

---
+++ resources/views/coaching.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="coaching-background-image">
+    <div class="w-4/5 m-auto text-center">
+        <div class="py-15 border-b border-gray-200">
+            <h1 class="text-6xl text-left" >
+                Unleash Your Inner Athlete
+            </h1>
+            <p class="text-6x1 text-left pt-6">
+                We are dedicated to helping you transform your body and mind through the power of fitness
+            </p>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection

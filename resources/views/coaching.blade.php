96f16cbdba00ad8a3113c225f5957f436ad425fa
Manin Macina <Feedesson12@gmail.com>
2023-05-08 16:11:43 +0100

    fix: coachig (CSS)

--- resources/views/coaching.blade.php
+++ resources/views/coaching.blade.php
@@ -1,22 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="coaching-background-image">
-    <div class="w-4/5 m-auto text-center">
-        <div class="pt-20 pb-10 max-w-lg">
-            <h1 class="text-6xl font-bold text-left text-white" >
-                Unleash Your Inner Athlete
-            </h1>
-            <p class="text-xl font-thin text-left pt-6 text-white">
-                We are dedicated to helping you transform your body and mind through the power of fitness
-            </p>
-        </div>
-            <button class="joinBtn">
-                Join now
-            </button>
-        </div>
+<div class="bg-black">
+    <div class="coaching-background-image">
+        <div class="w-4/5 m-auto">
+            <div class="pt-20 pb-10 max-w-lg">
+                <h1 class="text-6xl font-bold text-left text-white" >
+                    Unleash Your Inner Athlete
+                </h1>
+                <p class="text-xl font-thin text-left pt-6 text-white">
+                    We are dedicated to helping you transform your body and mind through the power of fitness
+                </p>
+            </div>
+                <button class="joinBtn">
+                    Join now
+                </button>
+            </div>
+    </div>
+    <div class="flex flex-row h-64 bg-black w-4/5 m-auto items-center justify-around">
+        <h2 class="text-5xl text-left font-black text-white max-w-md">
+            Transform Your Fitness Journey
+        </h2>
+        <p class="text-white text-justify font-thin text-xl max-w-lg">
+            We offer customised workout programs designed to help achieve your fitness goals, be it weight loss, strenght and conditioning or body shaping.
+        </p>
+    </div>
+    
 </div>
-
 
 
 

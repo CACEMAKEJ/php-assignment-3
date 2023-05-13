64161ae010af2b9746e1ca39503398dbfe8274e3
NojusMat <d00249674@student.dkit.ie>
2023-05-13 09:56:02 +0100

    suppliments page css

--- resources/views/supplements/supplements.blade.php
+++ resources/views/supplements/supplements.blade.php
@@ -1,17 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
 <div class="bg-black">
+
+{{--
+code gotten from https://tailwindui.com/components/application-ui/navigation/navbars --}}
+    <nav class="bg-yellow-300">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div class="relative flex h-16 items-center justify-between">
+            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+
+              <div class="hidden sm:ml-6 sm:block">
+                <div class="flex space-x-4">
+                  <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                  <a href="#" class="text-black hover:bg-yellow-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><i class="fa fa-truck"></i> Free delivery on products over 50€</a>
+                  <a href="#" class="text-black hover:bg-yellow-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><i class="fa fa-globe"></i> World leaders in Bodybuilding suppliments</a>
+                  <a href="#" class="text-black hover:bg-yellow-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium"> <i class="fa fa-school"></i> Students get free creatine when they sign up </a>
+                  <a href="#" class="text-black hover:bg-yellow-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><i class="fa fa-bolt-lightning"></i> Fast and easy checkout process</a>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+      </nav>
+
+
+
+
+
+
    <div class="grid gap-y-10 grid-cols-1 md:grid-cols-3 p-3" style="justify-items: center">
     {{-- code gotten from:  https://flowbite.com/docs/components/card/ --}}
-    <div class="w-full max-w-sm bg-black border border-yellow-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full max-w-sm bg-white border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <img class="p-8 rounded-t-md" src="\images\1.jpg" alt="product image" />
 
         </a>
         <div class="px-5 pb-5">
             <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight text-gray-100 dark:text-white">Depo-Testosterone</h5>
+                <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white border-b-2 border-yellow-300">Depo-Testosterone</h5>
             </a>
             <div class="flex items-center mt-2.5 mb-5">
                 <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -21,21 +51,21 @@
                 <span class="bg-yellow-100 text-black text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">4.0</span>
             </div>
             <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-yellow-300 dark:text-white"> Now $99.99</span>
-                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> $100.00</span>
+                <span class="text-3xl font-bold text-black dark:text-white"> Now €99.99</span>
+                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> €100.00</span>
 
                 <a href="#" class="text-black bg-yellow-300 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
             </div>
         </div>
     </div>
 
-    <div class="w-full max-w-sm bg-black border border-yellow-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full max-w-sm bg-white border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <img class="p-8 rounded-t-md" src="images\4.webp" alt="product image" />
         </a>
         <div class="px-5 pb-5">
             <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight text-gray-100 dark:text-white">Winstrol</h5>
+                <h5 class="text-xl font-semibold tracking-tight text-black dark:text-white border-b-2 border-yellow-300">Winstrol</h5>
             </a>
             <div class="flex items-center mt-2.5 mb-5">
                 <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -47,8 +77,8 @@
                 <span class="bg-yellow-100 text-black text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
             </div>
             <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-yellow-300 dark:text-white"> Now $49.99</span>
-                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> $130.00</span>
+                <span class="text-3xl font-bold text-black dark:text-white"> Now €49.99</span>
+                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> €130.00</span>
 
                 <a href="#" class="text-black bg-yellow-300 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
             </div>
@@ -56,13 +86,13 @@
     </div>
 
 
-    <div class="w-full max-w-sm bg-black border border-yellow-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full max-w-sm bg-white border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-            <img class="p-8 rounded-t-md" src="images/7.jpg" alt="product image" />
+            <img class="p-8 rounded-t-md" src="images/10.jpg" alt="product image" />
         </a>
         <div class="px-5 pb-5">
             <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight text-gray-100 dark:text-white">Anadrol</h5>
+                <h5 class="text-xl font-semibold tracking-tight text-black dark:text-white  border-b-2 border-yellow-300">Anadrol</h5>
             </a>
             <div class="flex items-center mt-2.5 mb-5">
                 <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -72,21 +102,21 @@
                 <span class="bg-yellow-100 text-black text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">4.0</span>
             </div>
             <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-yellow-300 dark:text-white"> Now $19.99</span>
-                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> $20.00</span>
+                <span class="text-3xl font-bold text-black dark:text-white"> Now €19.99</span>
+                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> €20.00</span>
 
                 <a href="#" class="text-black bg-yellow-300 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
             </div>
         </div>
     </div>
 
-    <div class="w-full max-w-sm bg-black border border-yellow-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full max-w-sm bg-white border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <img class="p-8 rounded-t-md" src="images\2.webp" alt="product image" />
         </a>
         <div class="px-5 pb-5">
             <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight text-gray-100 dark:text-white">Dianabol</h5>
+                <h5 class="text-xl font-semibold tracking-tight text-black dark:text-white border-b-2 border-yellow-300">Dianabol</h5>
             </a>
             <div class="flex items-center mt-2.5 mb-5">
                 <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -98,8 +128,8 @@
                 <span class="bg-yellow-100 text-black text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
             </div>
             <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-yellow-300 dark:text-white"> Now $59.99</span>
-                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> $60.00</span>
+                <span class="text-3xl font-bold text-black dark:text-white"> Now €59.99</span>
+                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> €60.00</span>
 
                 <a href="#" class="text-black bg-yellow-300 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
             </div>
@@ -107,13 +137,13 @@
     </div>
 
 
-    <div class="w-full max-w-sm bg-black border border-yellow-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full max-w-sm bg-white border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <img class="p-8 rounded-t-md" src="images/3.jpg" alt="product image" />
         </a>
         <div class="px-5 pb-5">
             <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight text-gray-100 dark:text-white">Oxandrin</h5>
+                <h5 class="text-xl font-semibold tracking-tight text-black dark:text-white border-b-2 border-yellow-300">Oxandrin</h5>
             </a>
             <div class="flex items-center mt-2.5 mb-5">
                 <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -125,23 +155,23 @@
                 <span class="bg-yellow-100 text-black text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
             </div>
             <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-yellow-300 dark:text-white"> Now $59.99</span>
-                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> $60.00</span>
+                <span class="text-3xl font-bold text-black dark:text-white"> Now €59.99</span>
+                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> €60.00</span>
 
                 <a href="#" class="text-black bg-yellow-300 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
             </div>
         </div>
     </div>
 
-   
 
-    <div class="w-full max-w-sm bg-black border border-yellow-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+    <div class="w-full max-w-sm bg-white border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <img class="p-8 rounded-t-md" src="images\5.jpg" alt="product image" />
         </a>
         <div class="px-5 pb-5">
             <a href="#">
-                <h5 class="text-xl font-semibold tracking-tight text-gray-100 dark:text-white">Equipoise</h5>
+                <h5 class="text-xl font-semibold tracking-tight text-black dark:text-white border-b-2 border-yellow-300">Equipoise</h5>
             </a>
             <div class="flex items-center mt-2.5 mb-5">
                 <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -151,14 +181,14 @@
                 <span class="bg-yellow-100 text-black text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">3.0</span>
             </div>
             <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-yellow-300 dark:text-white"> Now $10.49</span>
-                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-5 mr-3"> $20.00</span>
+                <span class="text-3xl font-bold text-black dark:text-white"> Now €10.49</span>
+                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-5 mr-3"> €20.00</span>
                 <a href="#" class="text-black bg-yellow-300 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
             </div>
         </div>
     </div>
 
-    <div class="w-full max-w-sm bg-black border border-yellow-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    {{-- <div class="w-full max-w-sm bg-black border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <img class="p-8 rounded-t-md" src="images\tren.jpg" alt="product image" />
         </a>
@@ -176,13 +206,13 @@
                 <span class="bg-yellow-100 text-black text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
             </div>
             <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold text-yellow-300 dark:text-white"> Now $159.99</span>
-                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> $200.00</span>
+                <span class="text-3xl font-bold text-yellow-300 dark:text-white"> Now €159.99</span>
+                <span class="text-2xl font-bold text-gray-500 dark:text-white line-through mr-3"> €200.00</span>
 
                 <a href="#" class="text-black bg-yellow-300 hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
             </div>
         </div>
-   </div>
+   </div> --}}
 
 </div>
 
